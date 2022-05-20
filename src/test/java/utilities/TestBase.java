442808953429529c67097e69db576ceb1b13091c
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-05-20 13:29:27 +0100

    Alert

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,41 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public  abstract class TestBase {
+
+    /*
+     * What is TestBase?
+     * TestBase is a class that has repeated pre condition and post condiditon
+     * We have @Before and @After methods
+     *
+     * TestBase should be an abstract class.We just want to implement the methods in TestBase
+     * We don't want to create an object od TestBase class
+     *
+     * */
+    protected WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+
+
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
+
+
+}
